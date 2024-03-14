57d878ab2018ae7449030586f2a9def177070c16
KyleM73 <kyle.morgenstein@gmail.com>
2024-03-14 16:29:14 -0500

    restructure

---
+++ hcrl_orbit/tasks/locomotion/velocity/mdp/actions.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import torch
+from collections.abc import Sequence
+from typing import TYPE_CHECKING
+from dataclasses import MISSING
+
+import carb
+
+from omni.isaac.orbit.utils import configclass
+import omni.isaac.orbit.utils.string as string_utils
+from omni.isaac.orbit.assets.articulation import Articulation
+from omni.isaac.orbit.managers.action_manager import ActionTerm, ActionTermCfg
+
+if TYPE_CHECKING:
+    from omni.isaac.orbit.envs import BaseEnv
+
+class WBCJointActionCfg:
+    pass
+
+class WBCJointAction(ActionTerm):
+    cfg: WBCJointActionCfg
+    """The configuration of the action term."""
+    _asset: Articulation
+    """The articulation asset on which the action term is applied."""
+    _scale: torch.Tensor | float
+    """The scaling factor applied to the input action."""
+    _offset: torch.Tensor | float
+    """The offset applied to the input action."""
+    def __init__(self, cfg: WBCJointActionCfg, env: BaseEnv) -> None:
+        # initialize the action term
+        super().__init__(cfg, env)
+
+        # resolve the joints over which the action term is applied
+        self._joint_ids, self._joint_names = self._asset.find_joints(self.cfg.joint_names)
+        self._num_joints = len(self._joint_ids)
+        # log the resolved joint names for debugging
+        carb.log_info(
+            f"Resolved joint names for the action term {self.__class__.__name__}:"
+            f" {self._joint_names} [{self._joint_ids}]"
+        )
+
+        # Avoid indexing across all joints for efficiency
+        if self._num_joints == self._asset.num_joints:
+            self._joint_ids = slice(None)
+            
+        # create tensors for raw and processed actions
+        self._raw_actions = torch.zeros(self.num_envs, self.action_dim, device=self.device)
+        self._processed_actions = torch.zeros_like(self.raw_actions)
+
+        # parse scale
+        if isinstance(cfg.scale, (float, int)):
+            self._scale = float(cfg.scale)
+        elif isinstance(cfg.scale, dict):
+            self._scale = torch.ones(self.num_envs, self.action_dim, device=self.device)
+            # resolve the dictionary config
+            index_list, _, value_list = string_utils.resolve_matching_names_values(self.cfg.scale, self._joint_names)
+            self._scale[:, index_list] = torch.tensor(value_list, device=self.device)
+        else:
+            raise ValueError(f"Unsupported scale type: {type(cfg.scale)}. Supported types are float and dict.")
+        # parse offset
+        if isinstance(cfg.offset, (float, int)):
+            self._offset = float(cfg.offset)
+        elif isinstance(cfg.offset, dict):
+            self._offset = torch.zeros_like(self._raw_actions)
+            # resolve the dictionary config
+            index_list, _, value_list = string_utils.resolve_matching_names_values(self.cfg.offset, self._joint_names)
+            self._offset[:, index_list] = torch.tensor(value_list, device=self.device)
+        else:
+            raise ValueError(f"Unsupported offset type: {type(cfg.offset)}. Supported types are float and dict.")
+
+    """
+    Properties.
+    """
