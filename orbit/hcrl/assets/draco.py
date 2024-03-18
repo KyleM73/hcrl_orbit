2b108afc84ddd08b78a556cf100c99fa5e37fdc2
KyleM73 <kyle.morgenstein@gmail.com>
2024-03-18 11:45:38 -0500

    draco gains

--- orbit/hcrl/assets/draco.py
+++ orbit/hcrl/assets/draco.py
@@ -26,7 +26,7 @@ ActuatorCfg = IdealPDActuatorCfg #ImplicitActuatorCfg
 DRACO_CFG = ArticulationCfg(
     spawn=sim_utils.UrdfFileCfg(
         fix_base=False,
-        merge_fixed_joints=True,
+        merge_fixed_joints=False,
         make_instanceable=True,
         asset_path=os.path.abspath(os.path.join(EXT_DIR, "resources/hcrl_robots/draco/draco.urdf")),
         activate_contact_sensors=True,
@@ -46,17 +46,17 @@ DRACO_CFG = ArticulationCfg(
         ),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.7),
+        pos=(0.0, 0.0, 7.43),
         joint_pos={
             "[lr]_hip_ie.*" : 0.000,
-            "l_hip_aa.*" : 0.160,
-            "r_hip_aa.*" : -0.160,
-            "[lr]_hip_fe.*" : -1.1, #-0.785,
+            "l_hip_aa.*" : 0.087, #0.160,
+            "r_hip_aa.*" : -0.087, #-0.160,
+            "[lr]_hip_fe.*" : -0.785, #-1.1, #-0.785,
             "[lr]_knee_fe_jp.*" : 0.785,
-            "[lr]_knee_fe_jd.*" : 1.1, #0.785,
-            "[lr]_ankle_fe.*" : -1.1, #-0.785,
-            "l_ankle_ie.*" : -0.160,
-            "r_ankle_ie.*" : 0.160,
+            "[lr]_knee_fe_jd.*" : 0.785, #1.1, #0.785,
+            "[lr]_ankle_fe.*" : -0.785, #-1.1, #-0.785,
+            "l_ankle_ie.*" : -0.087, #-0.160,
+            "r_ankle_ie.*" : 0.087, #0.160,
             "[lr]_shoulder_fe.*" : 0.000,
             "l_shoulder_aa.*" : 0.523,
             "r_shoulder_aa.*" : -0.523,
@@ -75,12 +75,12 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=8.80,
             velocity_limit=76.0,
             stiffness={
-                "neck_pitch.*": 15.0,
-                "[lr]_wrist_pitch.*": 7.0,
+                "neck_pitch.*": 0.0, #40.0, #15.0,
+                "[lr]_wrist_pitch.*": 0.0, #50.0, #7.0,
             },
             damping={
-                "neck_pitch.*": 0.0,
-                "[lr]_wrist_pitch.*": 0.07,
+                "neck_pitch.*": 0.0, #2.0, #0.0,
+                "[lr]_wrist_pitch.*": 0.0, #3.0, #0.07,
             },
             friction={
                 "neck_pitch.*": 0.0,
@@ -92,14 +92,14 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=18.0,
             velocity_limit=47.0,
             stiffness={
-                "[lr]_shoulder_fe.*": 250.0,
-                "[lr]_shoulder_aa.*": 300.0,
-                "[lr]_shoulder_ie.*": 60.0,
+                "[lr]_shoulder_fe.*": 0.0, #100.0, #250.0,
+                "[lr]_shoulder_aa.*": 0.0, #100.0, #300.0,
+                "[lr]_shoulder_ie.*": 0.0, #100.0, #60.0,
             },
             damping={
-                "[lr]_shoulder_fe.*": 3.0,
-                "[lr]_shoulder_aa.*": 3.0,
-                "[lr]_shoulder_ie.*": 2.0,
+                "[lr]_shoulder_fe.*": 0.0, #8.0, #3.0,
+                "[lr]_shoulder_aa.*": 0.0, #8.0, #3.0,
+                "[lr]_shoulder_ie.*": 0.0, #8.0, #2.0,
             },
             friction={
                 "[lr]_shoulder.*": 0.0,
@@ -110,10 +110,10 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=10.0,
             velocity_limit=59.2,
             stiffness={
-                "[lr]_elbow_fe.*": 100.0,
+                "[lr]_elbow_fe.*": 0.0, #50.0, #100.0,
             },
             damping={
-                "[lr]_elbow_fe.*": 1.2,
+                "[lr]_elbow_fe.*": 0.0, #3.0, #1.2,
             },
             friction={
                 "[lr]_elbow_fe.*": 0.0,
@@ -124,10 +124,10 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=12.0,
             velocity_limit=71.0,
             stiffness={
-                "[lr]_wrist_ps.*": 15.0,
+                "[lr]_wrist_ps.*": 0.0, #40.0, #15.0,
             },
             damping={
-                "[lr]_wrist_ps.*": 1.0,
+                "[lr]_wrist_ps.*": 0.0, #2.0, #1.0,
             },
             friction={
                 "[lr]_wrist_ps.*": 0.0,
@@ -138,12 +138,12 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=44.0,
             velocity_limit=8.1,
             stiffness={
-                "[lr]_hip_ie.*": 150.0,
-                "[lr]_ankle_fe.*": 200.0, #150.0,
+                "[lr]_hip_ie.*": 0.0, #150.0,
+                "[lr]_ankle_fe.*": 0.0, #200.0, #150.0,
             },
             damping={
-                "[lr]_hip_ie.*": 1.0,
-                "[lr]_ankle_fe.*": 2.0, #0.2,
+                "[lr]_hip_ie.*": 0.0, #1.0,
+                "[lr]_ankle_fe.*": 0.0, #2.0, #0.2,
             },
             friction={
                 "[lr]_hip_ie.*": 0.0,
@@ -155,10 +155,10 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=56.0,
             velocity_limit=5.1,
             stiffness={
-                "[lr]_hip_aa.*": 450.0,
+                "[lr]_hip_aa.*": 0.0, #450.0,
             },
             damping={
-                "[lr]_hip_aa.*": 5.0,
+                "[lr]_hip_aa.*": 0.0, #5.0,
             },
             friction={
                 "[lr]_hip_aa.*": 0.0,
@@ -169,27 +169,27 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=59.6,
             velocity_limit=36.7,
             stiffness={
-                "[lr]_hip_fe.*": 500.0,
+                "[lr]_hip_fe.*": 0.0, #500.0,
             },
             damping={
-                "[lr]_hip_fe.*": 5.0,
+                "[lr]_hip_fe.*": 0.0, #5.0,
             },
             friction={
                 "[lr]_hip_fe.*": 0.0,
             },
         ),
         "knee_fe": ActuatorCfg(
-            joint_names_expr=["[lr]_knee_fe.*"],
+            joint_names_expr=["[lr]_knee_fe_jp"],
             effort_limit=40.85,
             velocity_limit=13.35,
             stiffness={
-                "[lr]_knee_fe.*": 450.0, #450.0, # right: 450 difference from hardware
+                "[lr]_knee_fe_jp": 0.0, #450.0, #450.0, # right: 450 difference from hardware
             },
             damping={
-                "[lr]_knee_fe.*": 5.0, #1.5,
+                "[lr]_knee_fe_jp": 0.0, #5.0, #1.5,
             },
             friction={
-                "[lr]_knee_fe.*": 0.0,
+                "[lr]_knee_fe_jp": 0.0,
             },
         ),
         "ankle_ie": ActuatorCfg(
@@ -197,10 +197,10 @@ DRACO_CFG = ArticulationCfg(
             effort_limit=30.0,
             velocity_limit=11.1,
             stiffness={
-                "[lr]_ankle_ie.*": 30.0,
+                "[lr]_ankle_ie.*": 0.0, #30.0,
             },
             damping={
-                "[lr]_ankle_ie.*": 0.1,
+                "[lr]_ankle_ie.*": 0.0, #0.1,
             },
             friction={
                 "[lr]_ankle_ie.*": 0.0,
