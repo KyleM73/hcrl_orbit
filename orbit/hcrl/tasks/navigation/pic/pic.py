bafe777710e889107a7324bffc7d41d6ef1d4732
KyleM73 <kyle.morgenstein@gmail.com>
2024-04-06 10:59:56 -0500

    pass around sample ids -> fast

--- orbit/hcrl/tasks/navigation/pic/pic.py
+++ orbit/hcrl/tasks/navigation/pic/pic.py
@@ -29,7 +29,7 @@ class PathIntegralController:
         self.k1 = -self.e / self.T
         self.k2, self.k3 = self.k1, self.k1
         self.s = self.s2**0.5
-        self.G_u = torch.tensor([[0, 0], [0, 0], [1, 0], [0, 1]], dtype=torch.float64, device=self.device)
+        self.G_u = torch.tensor([[0, 0], [0, 0], [1, 0], [0, 1]], dtype=torch.float32, device=self.device)
 
         # tracking objects
         self.reset(obs)
@@ -40,17 +40,19 @@ class PathIntegralController:
         self.x_sampled[self.step] = self.x
         self.S_tau[:] = 0
         self.collision_flag[:] = 1.0
-        self.eps_sampled = torch.randn(self.num_steps-self.step, self.num_samples, 2, 1, dtype=torch.float64, device=self.device)
+        sample_ids = torch.argwhere(self.collision_flag)[:, 0]
+        self.eps_sampled = torch.randn(self.num_steps-self.step, self.num_samples, 2, 1, dtype=torch.float32, device=self.device)
         
         for i in range(self.step, self.num_steps):
-            self.S_tau += self.collision_flag * self.dt * self.b * torch.square(torch.norm(self.x_sampled[i, :, :2], dim=1))
-            noisy_control = torch.einsum("ij,ajk->aik", self.G_u, self.s * self.eps_sampled[i-self.num_steps] * self.dt**0.5)
-            self.x_sampled[i+1] = self.x_sampled[i] + (self.f(self.x_sampled[i]) * self.dt + noisy_control) * self.collision_flag.repeat(1, 4).unsqueeze(-1)
+            self.S_tau[sample_ids] += self.dt * self.b * torch.square(torch.norm(self.x_sampled[i, sample_ids, :2], dim=1))
+            noisy_control = torch.einsum("ij,ajk->aik", self.G_u, self.s * self.eps_sampled[i-self.num_steps, sample_ids] * self.dt**0.5)
+            self.x_sampled[i+1, sample_ids] = self.x_sampled[i, sample_ids] + self.f(self.x_sampled[i, sample_ids]) * self.dt + noisy_control
             self.collision_flag, self.S_tau = self.update_collision(
                 obs, self.x_sampled[i+1, :, 0], self.x_sampled[i+1, :, 1], self.collision_flag, self.S_tau)
-        self.S_tau += torch.where(self.collision_flag > 0.9, self.d * torch.square(torch.norm(self.x_sampled[self.num_steps, :, :2], dim=1)), 0.0)
+            sample_ids = torch.argwhere(self.collision_flag)[:, 0]
+        self.S_tau[sample_ids] += self.d * torch.square(torch.norm(self.x_sampled[self.num_steps, sample_ids, :2], dim=1))
 
-        denom_i = torch.exp(-self.S_tau / self.lambd) + 1e-5 # avoid divide by zero for very large costs
+        denom_i = torch.exp(-self.S_tau / self.lambd) #+ 1e-5 # avoid divide by zero for very large costs
         numer = torch.einsum("ijk,ik->jk",self.eps_sampled[0], denom_i)
         denom = torch.sum(denom_i)
 
@@ -64,12 +66,12 @@ class PathIntegralController:
 
     def reset(self, obs: torch.Tensor) -> None:
         self.x = self.get_state(obs) # x1, x2, v, theta
-        self.eps = torch.randn(self.num_steps+1, 2, 1, dtype=torch.float64, device=self.device)
-        self.x_sampled = torch.zeros(self.num_steps+1, self.num_samples, 4, 1, dtype=torch.float64, device=self.device)
-        self.f_const = torch.diag(torch.tensor([self.k1, self.k1, self.k2, self.k3], dtype=torch.float64, device=self.device))
-        self.eps_sampled = torch.randn(self.num_steps+1, self.num_samples, 2, 1, dtype=torch.float64, device=self.device)
-        self.S_tau = torch.zeros(self.num_samples, 1, dtype=torch.float64, device=self.device)
-        self.collision_flag = torch.ones(self.num_samples, 1, dtype=torch.float64, device=self.device)
+        self.eps = torch.randn(self.num_steps+1, 2, 1, dtype=torch.float32, device=self.device)
+        self.x_sampled = torch.zeros(self.num_steps+1, self.num_samples, 4, 1, dtype=torch.float32, device=self.device)
+        self.f_const = torch.diag(torch.tensor([self.k1, self.k1, self.k2, self.k3], dtype=torch.float32, device=self.device))
+        self.eps_sampled = torch.randn(self.num_steps+1, self.num_samples, 2, 1, dtype=torch.float32, device=self.device)
+        self.S_tau = torch.zeros(self.num_samples, 1, dtype=torch.float32, device=self.device)
+        self.collision_flag = torch.ones(self.num_samples, 1, dtype=torch.float32, device=self.device)
         self.step = 0
 
     def f(self, x: torch.Tensor) -> torch.Tensor:
@@ -80,7 +82,7 @@ class PathIntegralController:
 
     def get_state(self, obs: torch.Tensor) -> torch.Tensor:
         # [x, y, s, theta]
-        return torch.tensor([[obs[:, 0]], [obs[:, 1]], [obs[:, 4:6].norm(dim=1)], [obs[:, 3]]], dtype=torch.float64, device=self.device).view(-1, 4, 1)
+        return torch.tensor([[obs[:, 0]], [obs[:, 1]], [obs[:, 4:6].norm(dim=1)], [obs[:, 3]]], dtype=torch.float32, device=self.device).view(-1, 4, 1)
     
     def get_box_points(self, obs: torch.Tensor) -> List[torch.Tensor]:
         assert obs.size(1) > 10
@@ -106,7 +108,12 @@ class PathIntegralController:
         S_tau += torch.where(collision_flag < 0.9, penalty, 0.0)
         return collision_flag, S_tau
     
-    def check_collision(self, obs: torch.Tensor, x: torch.Tensor, y: torch.Tensor, collision_flag: torch.Tensor):
+    def check_collision(self,
+                        obs: torch.Tensor,
+                        x: torch.Tensor,
+                        y: torch.Tensor,
+                        collision_flag: torch.Tensor,
+                        ) -> torch.Tensor:
         # check in bounds
         inside_border = (
             torch.where((-self.border_radius < x) & ( x < self.border_radius), True, False)) & \
@@ -135,13 +142,14 @@ if __name__ == "__main__":
         0, 0, 0,   # ang vel
         #2, 2, 0,   # box pose
         -2, -2, 0, # box pose
-    ]).view(1, -1).to(dtype=torch.float64, device=device)
+    ]).view(1, -1).to(dtype=torch.float32, device=device)
     dt, T = 0.1, 10
     num_samples = 10000
 
     num_trajs = 100
     fails = 0
     for _ in range(num_trajs):
+        print("traj: ", _)
         policy = PathIntegralController(obs, dt, T, num_samples, device=device)
         collision_flag = 1.0
         for i in range(int(T/dt)-1):
@@ -151,7 +159,7 @@ if __name__ == "__main__":
                 print("collision")
                 fails += 1
                 break
-            print(x)
+            #print(x)
         print("Final State: ",x[0, ...])
         print("Error: ",torch.norm(x[:, :2]))
     print("Success Rate: ",1-fails/num_trajs)
