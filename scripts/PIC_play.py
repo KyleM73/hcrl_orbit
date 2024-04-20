cecc831e45878ab3bbb7301f5244ec0b17ed287f
KyleM73 <kyle.morgenstein@gmail.com>
2024-04-20 16:03:46 -0500

    working sim for orbit v0.3 + P tracking controller

--- scripts/PIC_play.py
+++ scripts/PIC_play.py
@@ -13,7 +13,7 @@ from omni.isaac.orbit.app import AppLauncher
 parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
 parser.add_argument("--video_length", type=int, default=1000, help="Length of the recorded video (in steps).")
-parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
+parser.add_argument("--video_interval", type=int, default=20000, help="Interval between video recordings (in steps).")
 parser.add_argument("--cpu", action="store_true", default=False, help="Use CPU pipeline.")
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
@@ -25,13 +25,13 @@ AppLauncher.add_app_launcher_args(parser)
 args_cli = parser.parse_args()
 
 # load cheaper kit config in headless
-if args_cli.headless:
-    app_experience = f"{os.environ['EXP_PATH']}/omni.isaac.sim.python.gym.headless.kit"
-else:
-    app_experience = f"{os.environ['EXP_PATH']}/omni.isaac.sim.python.kit"
+#if args_cli.headless:
+#    app_experience = f"{os.environ['EXP_PATH']}/omni.isaac.sim.python.gym.headless.kit"
+#else:
+#    app_experience = f"{os.environ['EXP_PATH']}/omni.isaac.sim.python.kit"
 
 # launch omniverse app
-app_launcher = AppLauncher(args_cli, experience=app_experience)
+app_launcher = AppLauncher(args_cli)#, experience=app_experience)
 simulation_app = app_launcher.app
 
 """Rest everything follows."""
@@ -50,6 +50,7 @@ from omni.isaac.orbit.utils.io import dump_pickle, dump_yaml
 
 import omni.isaac.orbit.sim as sim_utils
 from omni.isaac.orbit.markers import VisualizationMarkersCfg, VisualizationMarkers
+from omni.isaac.orbit.utils.math import wrap_to_pi
 
 import orbit.hcrl  # noqa: F401
 import omni.isaac.orbit_tasks  # noqa: F401
@@ -60,13 +61,15 @@ if args_cli.plot:
     import matplotlib.pyplot as plt
 
 def PDcontroller(state, obs, device, P_vel=1, P_theta=1):
-    pose_d, speed_d, theta_d = state[:, :2, 0], state[:, 2, 0], state[:, 3, 0]
+    pose_d, _, theta_d = state[:, :2, 0], state[:, 2, 0], state[:, 3, 0]
     pose_diff = pose_d - obs[:, :2]
-    theta = torch.arctan2(pose_diff[:, 0], pose_diff[:, 1])
-    theta_err = theta - obs[:, 3]
-
-    action = torch.tensor([P_vel * speed_d, P_theta * theta_err], device=device).view(-1, 2)
-    return action
+    theta_d = torch.arctan2(pose_diff[:, 1], pose_diff[:, 0])
+    theta_err = wrap_to_pi(theta_d - obs[:, 3])
+    return torch.tensor([
+        P_vel * pose_diff[:, 0] * torch.cos(theta_d),
+        P_vel * pose_diff[:, 1] * torch.sin(theta_d),
+        P_theta * theta_err
+        ], device=device).view(-1, 3)
 
 def main():
     """Train with RSL-RL agent."""
@@ -110,70 +113,88 @@ def main():
     step = 0
 
     # obtain the policy for inference
-    obs[0, :2] = 4
-    init_pose = obs[0, :2]
-    pose = torch.zeros(1, 3, device=env.unwrapped.device)
-    dt = 0.01
-    T = args_cli.video_length * dt
-    policy = orbit.hcrl.tasks.navigation.pic.PathIntegralController(obs, dt, T, num_samples=10_000, box_radius=0.5, device=env.unwrapped.device)
+    obs[0, :2] = 5
+    traj_pts = obs[:, :3].clone()
+    dt = 0.1
+    decimation = int(dt / env.unwrapped.step_dt)
+    T = args_cli.video_length * dt / decimation
+    box_radius = 0.5/2 + 0.3 #c-space
+    policy = orbit.hcrl.tasks.navigation.pic.PathIntegralController(obs, dt, T, num_samples=10_000, box_radius=box_radius, device=env.unwrapped.device)
 
     cfg = VisualizationMarkersCfg(
         prim_path="/World/Visuals/testMarkers",
         markers={
             "sample": sim_utils.SphereCfg(
-                radius=0.1,
-                visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.2, 0.2, 0.2)),
+                radius=0.02,
+                visual_material=sim_utils.PreviewSurfaceCfg(
+                    diffuse_color=(1.0, 0.0, 0.0),
+                    emissive_color=(0.0, 0.0, 0.0),
+                    roughness=0.0,
+                    metallic=0.0,
+                    opacity=1.0
+                    ),
             ),
-            "true": sim_utils.SphereCfg(
-                radius=0.3,
-                visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(1.0, 0.0, 0.0)),
+            "trajectory": sim_utils.SphereCfg(
+                radius=0.02,
+                visual_material=sim_utils.PreviewSurfaceCfg(
+                    diffuse_color=(0.0, 0.0, 1.0),
+                    emissive_color=(0.0, 0.0, 0.0),
+                    roughness=0.0,
+                    metallic=0.0,
+                    opacity=1.0
+                ),
             ),
+            "state": sim_utils.SphereCfg(
+                radius=0.2,
+                visual_material=sim_utils.PreviewSurfaceCfg(
+                    diffuse_color=(0.0, 1.0, 0.0),
+                    emissive_color=(0.0, 0.0, 0.0),
+                    roughness=0.0,
+                    metallic=0.0,
+                    opacity=1.0
+                ),
+            )
         }
     )
     marker = VisualizationMarkers(cfg)
     
     # simulate environment
     while simulation_app.is_running():
+        if not step % 50: print("Step {}/{}...".format(step, args_cli.video_length))
         # run everything in inference mode
         with torch.inference_mode():
             # agent stepping
-            #state, actions, samples = policy(obs)
-            state = torch.tensor([3.9, 3.9, 1, 3.14], device=env.unwrapped.device).view(1, 4, 1)
-            action = PDcontroller(state, obs, env.unwrapped.device, 0, 1)
-            print(action)
-            
-
-            #pose[0, :2] = state[0, :2, 0]# - init_pose
-            #print(state[:, :2, 0])
-            #print(init_pose)
-            #print(obs[:, :2])
-            #print(pose)
-            #print()
+            state, _, samples = policy(obs)
             # visualize samples
-            #pts = samples[::100, ::100, :3, 0] #sample every 10th point
-            #pts[:, :, 2] = 0.2
-            #pts = pts.flatten(0, 1)
-            #x = state[:, :3, 0]
-            #x[:, 2] = 0.3
-            #pts = torch.cat((pts, x.view(-1, 3)), dim=0)
-            #marker_indices = [0] * pts.size(0) + [1] * 1
-            #marker.visualize(translations=pts) #marker_indices=marker_indices
+            sample_pts = samples[:, ::100, :3, 0] #sample every 500th trajectory
+            sample_pts = sample_pts.flatten(0, 1)
+            #print(sample_pts.size())
+            traj_pts = torch.cat((traj_pts, obs[:, :3]), dim=0)
+            #pts = torch.cat((sample_pts, traj_pts, state[:, :3, 0]), dim=0)
+            pts = torch.cat((sample_pts, traj_pts), dim=0)
+            pts[:, 2] = 0.2
+            marker_indices = [0] * sample_pts.size(0) + [1] * traj_pts.size(0)# + [2] * 1
             # env stepping
-            #state, actions, samples = policy(obs)
-            #obs, _, _, _ = env.step(actions.view(1, 2))
-            #pose = torch.zeros(1, 2, device=env.unwrapped.device)
-            #pose[:, :] = -1
-            obs, _, _, _ = env.step(action)
-            print(obs[:, :3])
+            print("state ",state)
+            for _ in range(decimation):
+                action = PDcontroller(state, obs, env.unwrapped.device, P_vel=5, P_theta=10)
+                collision_flag = policy.check_collision(obs, state[:, 0, :], state[:, 1, :], collision_flag=1.0)
+                if collision_flag < 0.9:
+                    print("collision")
+                    assert False
+                obs, _, _, _ = env.step(action)
+                marker.visualize(translations=pts, marker_indices=marker_indices)
+                env.unwrapped.render()
+                step += 1
+            if step+1 >= args_cli.video_length: break
+            if env.unwrapped.sim.is_stopped(): break
+            print("obs ",obs)
             print()
-            env.unwrapped.render()
-        if not step % 50: print("Step {}/{}...".format(step, args_cli.video_length))
-        if step >= args_cli.video_length-1: break
-        if env.unwrapped.sim.is_stopped(): break
-        step += 1
-
+    print("Step {}/{}...".format(args_cli.video_length, args_cli.video_length))
+        
     # close the simulator
     env.close()
+    print("Video saved to ",log_dir)
 
 if __name__ == "__main__":
     # run the main function
